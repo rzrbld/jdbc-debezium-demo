b5de224a7499cf7dba8e558043392da82cc6e6f3
Aleksandr Petruhin <rzrbld@users.noreply.github.com>
2018-03-28 02:43:15 +0300

    Update rundemo.sh

--- rundemo.sh
+++ rundemo.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 mkdir -p /opt/debezium && \
+curl -fSL -o /tmp/postinstall.zip \
+             https://github.com/rzrbld/jdbc-debezium-postinstall/archive/master.zip &&\
+mkdir /tmp/postinstall && \
+unzip /tmp/postinstall.zip -d /tmp/postinstall/ &&\
+rm -rf /tmp/postinstall.zip &&\
+chmod +x /tmp/postinstall/jdbc-debezium-postinstall-master/register-connectors.sh && \
 echo "run demo" && \
 byobu start-server && \
 byobu new-session -d -s debeziumdemo -n docker &&\
 byobu new-window -t debeziumdemo:1 -n kafka &&\
 byobu new-window -t debeziumdemo:2 -n postgres &&\
 byobu new-window -t debeziumdemo:3 -n sqlite &&\
+byobu new-window -t debeziumdemo:4 -n post &&\
 tmux send-keys -t debeziumdemo:0 "export DEBEZIUM_VERSION=0.7 && cd /opt/debezium; docker-compose -f docker-compose-debezium-demo.yaml up " C-m
 sleep 70 &&
+tmux send-keys -t debeziumdemo:3 "./tmp/postinstall/jdbc-debezium-postinstall-master/register-connectors.sh" C-m
 tmux send-keys -t debeziumdemo:1 "export DEBEZIUM_VERSION=0.7 && cd /opt/debezium; docker-compose -f docker-compose-debezium-demo.yaml exec kafka /kafka/bin/kafka-console-consumer.sh --bootstrap-server kafka:9092 --from-beginning --property print.key=true --topic customers " C-m
 tmux send-keys -t debeziumdemo:2 "export DEBEZIUM_VERSION=0.7 && cd /opt/debezium; docker-compose -f docker-compose-debezium-demo.yaml exec postgres env PGOPTIONS=\"--search_path=inventory\" bash -c 'psql -U $POSTGRES_USER postgres' " C-m
 tmux send-keys -t debeziumdemo:3 "export DEBEZIUM_VERSION=0.7 && cd /opt/debezium; docker-compose -f docker-compose-debezium-demo.yaml exec connect sqlite3 /kafka/consumers.db" C-m
